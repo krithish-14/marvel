789878b7f86b8de10de6da21a6d915375ed35792
krithish-14 <krithish1445@gmail.com>
2026-04-01 00:29:49 +0530

    fix: resolve vercel missing django entrypoint error by adding vercel.json, wsgi alias, and requirements.txt

--- marvel_site/settings.py
+++ marvel_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-na7r-$b05fa=f6w7jvn8!20m*32#w4r-bk*ze@jq=0qtriy$4&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
