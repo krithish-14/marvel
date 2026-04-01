58fdae34d4f0b690885ab18129cd1de9fd79fdd5
krithish-14 <krithish1445@gmail.com>
2026-04-01 23:36:35 +0530

    fix: remove build_files.sh to force Vercel Zero-Config Python deployment (fixes PEP 668 and ModuleNotFound errors)

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo 'Starting Build...'
-python3 -m pip install -r requirements.txt --break-system-packages
-echo 'Collecting Static Files...'
-python3 manage.py collectstatic --noinput
-echo 'Running Migrations...'
-python3 manage.py migrate
-echo 'Build Completed.'
