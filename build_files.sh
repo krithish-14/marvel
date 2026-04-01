c65dcc11f618cbec90433caac6cf0f5880a6169a
krithish-14 <krithish1445@gmail.com>
2026-04-01 22:48:45 +0530

    fix: add --break-system-packages to pip, add buildCommand to vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput
 python manage.py migrate
